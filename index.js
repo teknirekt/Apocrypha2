085014cdb610f04a4dd86aab9c4d98da9189955a
Teknirekt <48459643+teknirekt@users.noreply.github.com>
2020-03-18 10:50:01 -0500

    fix indent

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ client.commands = new Discord.Collection();
 
 //FILESYSTEM
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-};
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 //EVENT HANDLER
 fs.readdir('./events/', (err, files) => {
-    if (err) return console.error;
-    files.forEach(file => {
-        if (!file.endsWith('.js')) return;
-        const evt = require(`./events/${file}`);
-        const evtName = file.split('.')[0];
-        console.log(`Loaded ${evtName}.`);
-        client.on(evtName, evt.bind(null, client));
-    });
+	if (err) return console.error;
+	files.forEach(file => {
+		if (!file.endsWith('.js')) return;
+		const evt = require(`./events/${file}`);
+		const evtName = file.split('.')[0];
+		console.log(`Loaded ${evtName}.`);
+		client.on(evtName, evt.bind(null, client));
+	});
 });
 //ERROR REPORTING
 client.on('error', error => console.log(error));

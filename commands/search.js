fbe4bb006f987228f59e8087e307306f95c0aea6
Teknirekt <teknirekt@gmail.com>
2020-03-24 11:15:04 -0500

    profile added, search updated

--- commands/search.js
+++ commands/search.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const request = require('request');
+// const fetch = require('node-fetch');
+// const Discord = require('discord.js');
 
 module.exports = {
 	name: 'search',
@@ -8,19 +9,33 @@ module.exports = {
 	description: 'Find information on a Destiny 2 player.',
 	usage: ['Teknirekt'],
 	category: 'owner',
-	execute(message, args) {
+	async execute(message, args) {
 
-		var options = {
-			'method': 'GET',
-			'url': `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/${args[0]}`,
-			'headers': {
-				'x-api-key': process.env.BUNGO_KEY
-			}
-		};
-		request(options, function (error, response) {
-			if (error) throw new Error(error);
-			message.channel.send(response.body);
-		});
+		message.channel.send('This command is still a WIP. I\'m unable to search for' + args[0] + 'currently. Sorry!')
+			.then(message => { 
+				if (message.channel.name !== 'landing') {
+					message.delete({ timeout: 30000 });
+				}
+			}).catch(console.error);
+		return;
+
+		// const url = 'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/';
+		// const headers = {
+		// 	'x-api-key': process.env.BUNGO_KEY
+		// };
+		// const response = await fetch(url + args[0], { method: 'GET', headers: headers });
+		// const data = await response.json();
+			
+		// const body = data.body;
+		// // const name = body.find(post => post.displayName === args[0]);
+
+		// // const memberInfo = new Discord.MessageEmbed()
+		// // 	.setTitle(name.displayName)
+		// // 	.setThumbnail(`https://www.bungie.net/Platform/${name.iconPath}`)
+		// // 	.addField('__**Member\'s Unique ID**__', name.membershipId);
+
+		// // message.channel.send(memberInfo);
+		// console.log(body);
 	}
 };
 

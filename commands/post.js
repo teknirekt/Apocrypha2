5e5c9f709da4ccbcee9bd418e8a8a541abccfe18
Teknirekt <teknirekt@gmail.com>
2020-03-26 18:59:34 -0500

    added extras

--- commands/post.js
+++ commands/post.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'post',
 	category: 'mod',
@@ -10,8 +12,6 @@ module.exports = {
 		//SERVERS
 		const backup = message.client.guilds.cache.get('608709068776931328');
 		const live = message.client.guilds.cache.get('229275151437529088');
-		// const test = message.client.guilds.cache.get('569908733090594827');
-		// const emoji = message.client.guilds.cache.get('613401400654692387');
 	
 		//CHANNELS
 		const rules = live.channels.cache.get('635885093885575178');
@@ -32,9 +32,6 @@ module.exports = {
 		const rule9 = await rules.messages.fetch('690618924009783376');
 		const rule10 = await rules.messages.fetch('690618966422585374');
 		const rule11 = await rules.messages.fetch('690619086949842964');
-		// const rule12 = await rules.messages.fetch('690619131585626203');
-		// const rule13 = await rules.messages.fetch('690619181657358397');
-		// const rule14 = await rules.messages.fetch('690619240205647913');
 		const ruleset1 = await rules.messages.fetch('635885183471714336');
 		const ruleset2 = await rules.messages.fetch('635885244280733746');
 
@@ -95,24 +92,36 @@ module.exports = {
 			message.channel.send(ruleset1.content).then(message.channel.send(ruleset2.content));
 			return;
 		}
-		// if (args[0] === 'rule12') {
-		// 	message.channel.send(rule12.content);
-		// 	return;
-		// }
-		// if (args[0] === 'rule13') {
-		// 	message.channel.send(rule13.content);
-		// 	return;
-		// }
-		// if (args[0] === 'rule14') {
-		// 	message.channel.send(rule14.content);
-		// 	return;
-		// }
 		if (args[0] === 'edit') {
 
 			const fetched = await message.channel.messages.fetch(args[1]);
 			const content = args.slice(2).join(' ');
 
-			fetched.edit(content).catch(console.error);
+			if (message.channel.name === 'destiny_guides') {
+				if (message.author.id !== '229060782065844224') {
+					message.channel.send('Nope, Sorry. Only Tek can do that.')
+						.then(message.delete({ timeout: 10000 }));
+					return;
+				} else {
+					
+					const extras = new Discord.MessageEmbed()
+						.setColor('#545454')
+						.setTitle('Extra Resources Available')
+						.setURL('https://www.reddit.com/r/DestinyTheGame/comments/dd7fh3/in_the_spirit_of_new_light_heres_a_sizeable_list/')
+						.setThumbnail('https://i.ibb.co/wYvZtB4/DStiq-SBWs-AAIM7-C.jpg')
+						.addFields(
+							{ name: '__**Reddit Sources**__', value: '[**r/DestinyTheGame**](https://www.reddit.com/r/DestinyTheGame/) -General Info\n[**r/RaidSecrets**](https://www.reddit.com/r/raidsecrets/) -Spoiler Heavy\n[**r/CruciblePlaybook**](https://new.reddit.com/r/CruciblePlaybook/) -Everything PVP\n[**r/GambitPlaybook**](https://www.reddit.com/r/GambitPlaybook/) -Everything Gambit\n[**r/DestinyLore**](https://www.reddit.com/r/DestinyLore/) -Everything Lore' },
+							{ name: '__**Websites**__', value: '[**DestinyItemManager(DIM)**](https://app.destinyitemmanager.com/)\n[**Destiny Sets**](https://destinysets.com/)\n[**Interactive Director**](https://lowlidev.com.au/destiny/maps/)\n[**Light.gg**](https://light.gg/)\n[**Braytech.org**](https://braytech.org/)\n[**Where\'s Xur?**](https://ftw.in/game/destiny-2/find-xur)\n[**D2 Gunsmith**](https://d2gunsmith.com/)\n[**Today In Destiny**](https://www.todayindestiny.com/)', inline: true },
+							{ name: '__**Mobile Apps & Misc**__', value: '[**Redrix**](https://redrix.io/) 📱\n[**God Roll Spreadsheet**](https://bit.ly/3bmIsHz)\n[**Weapon Perks**](https://bit.ly/2Jh8akP)', inline: true },
+							{ name: '__**Soundboards**__', value: '[**Shaxx Soundboard**](https://evilarceus.github.io/Destiny2-Soundboard/#)\n[**Drifter Soundboard**](https://d2.asun.co/drifter/)' }
+						)
+						.setImage('https://i.ibb.co/k2X3xkV/extra2.png')
+						.setFooter('Command: -g extras');
+
+					fetched.edit(extras);
+				}
+			} else
+				fetched.edit(content).catch(console.error);
 			return;
 		}
 	}

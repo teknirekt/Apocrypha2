f4510fbc86b7704f03ec08cf60eb2eca53eba75a
Teknirekt <48459643+teknirekt@users.noreply.github.com>
2021-02-15 20:22:09 -0600

    added hiatus role

--- commands/kicklist.js
+++ commands/kicklist.js
@@ -10,17 +10,20 @@ module.exports = {
 		await message.guild.members.fetch().then(fetchedMembers => {
 			const visitor = fetchedMembers.filter(member => member.roles.cache.some(r => r.name === 'Visitor'));
 			const inactive = fetchedMembers.filter(member => member.roles.cache.some(r => r.name === 'INACTIVE'));
+			const hiatus = fetchedMembers.filter(member => member.roles.cache.some(r => r.name === 'HIATUS'));
 			const missed = fetchedMembers.filter(member => member.roles.cache.some(r => r.name === 'RΞDΛCTΞD') && !member.roles.cache.some(r => r.name === 'IronWolves'));
 			const visitorList = visitor.map(member => member.user.toString()).join(', ');
 			const inactiveList = inactive.map(member => member.user.toString()).join(', ');
+			const hiatusList = hiatus.map(member => member.user.toString()).join(', ');
 			const missedList = missed.map(member => member.user.toString()).join(', ');
 
 			const kicklist = new Discord.MessageEmbed()
 				.setTitle('The list of members in each category.')
 				.addFields([
-					{ name: `__**Visitors**__ (${visitor.size})`, value: visitorList ? visitorList : 'All Clear!' },
+					{ name: `__**Discord Visitors**__ (${visitor.size})`, value: visitorList ? visitorList : 'All Clear!' },
 					{ name: `__**Inactive Members**__ (${inactive.size})`, value: inactiveList ? inactiveList : 'All Clear!' },
-					{ name: `__**Members Need Adjustment**__ (${missed.size})`, value: missedList ? missedList : 'All Clear!' }
+					{ name: `__**On Hiatus**__ (${hiatus.size})`, value: hiatusList ? hiatusList : 'All Clear!' },
+					{ name: `__**Need Roles Reviewed**__ (${missed.size})`, value: missedList ? missedList : 'All Clear!' }
 				])
 				.setColor('631B99');
 

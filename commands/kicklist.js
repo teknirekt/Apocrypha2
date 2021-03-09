0279c79d5d6c879e3d49384c778cbce7bbb62a46
Teknirekt <48459643+teknirekt@users.noreply.github.com>
2021-03-09 00:36:40 -0600

    Add files via upload
    fixed mentions & left

--- commands/kicklist.js
+++ commands/kicklist.js
@@ -13,11 +13,12 @@ module.exports = {
 			const hiatus = fetchedMembers.filter(member => member.roles.cache.some(r => r.name === 'HIATUS'));
 			const missed = fetchedMembers.filter(member => member.roles.cache.some(r => r.name === 'RΞDΛCTΞD') && !member.roles.cache.some(r => r.name === 'IronWolves'));
 			const left = fetchedMembers.filter(member => member.roles.cache.some(r => r.name === 'IronWolves') && !member.roles.cache.some(r => r.name === 'RΞDΛCTΞD'));
-			const visitorList = visitor.map(member => member.user.toString()).join(', ');
-			const inactiveList = inactive.map(member => member.user.toString()).join(', ');
-			const hiatusList = hiatus.map(member => member.user.toString()).join(', ');
-			const missedList = missed.map(member => member.user.toString()).join(', ');
-			const leftList = left.map(member => member.user.toString()).join(', ');
+			const filteredLeft = left.filter(member => !member.roles.cache.some(r => r.name === 'Honorary Access') && !member.roles.cache.some(r => r.name === 'Cayde\'sMafia'));
+			const visitorList = visitor.map(member => member.toString()).join(', ');
+			const inactiveList = inactive.map(member => member.toString()).join(', ');
+			const hiatusList = hiatus.map(member => member.toString()).join(', ');
+			const missedList = missed.map(member => member.toString()).join(', ');
+			const leftList = filteredLeft.map(member => member.toString()).join(', ');
 
 			const kicklist = new Discord.MessageEmbed()
 				.setTitle('The list of members in each category.')
@@ -25,7 +26,7 @@ module.exports = {
 					{ name: `__**Discord Visitors**__ (${visitor.size})`, value: visitorList ? visitorList : 'No visitors remain.' },
 					{ name: `__**Inactive Members**__ (${inactive.size})`, value: inactiveList ? inactiveList : 'Everyone is active.' },
 					{ name: `__**On Hiatus**__ (${hiatus.size})`, value: hiatusList ? hiatusList : 'None are on hiatus.' },
-					{ name: `__**Left the Community**__ (${left.size})`, value: leftList ? leftList : 'Everyone is a member.' },
+					{ name: `__**Left the Community**__ (${filteredLeft.size})`, value: leftList ? leftList : 'Everyone is a member.' },
 					{ name: `__**Needs Roles Reviewed**__ (${missed.size})`, value: missedList ? missedList : 'All roles appear correct.' }
 				])
 				.setColor('631B99');
